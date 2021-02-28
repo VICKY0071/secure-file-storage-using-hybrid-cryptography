afee570c058c7de48729f38615c07138f24de507
VICKY0071 <alexrocko90@gmail.com>
2021-02-28 05:57:06 +0530

    added a source code to convert vide files to base64

---
+++ video to base64.py
@@ -0,0 +1,18 @@
+import base64
+
+class mp4ToBase64:
+	def __init__(self, file):
+		self.text = base64.b64encode(file.read())
+
+	def to_64(self):
+		return self.text
+
+	def to_mp4(self, t):
+		file = open('output_video.mp4', 'wb')
+		file.write(base64.b64decode(t))
+		file.close()
+
+
+x = mp4ToBase64(open('video file.mp4', 'rb'))
+
+x.to_mp4(x.to_64())
