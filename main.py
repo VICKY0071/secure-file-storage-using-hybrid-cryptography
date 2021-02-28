b66d61db64486b686d055c10c56bf09f7e1b0b81
VICKY0071 <alexrocko90@gmail.com>
2021-02-28 06:49:21 +0530

    performing some modifications

--- main.py
+++ main.py
@@ -2,8 +2,21 @@ import os, sys
 from AESfile import AESCipher
 from DESfile import DESCipher
 from RC4file import RC4Cipher
+from videotobase64 import mp4ToBase64
 
-if __name__ == '__main__':
-    a = AESCipher("Vikas")
-    cipher = a.encrypt(a)
-    message = a.decrypt(cipher)   
+def encrypt_text(file):
+    ss = file.read()
+    s1 = ss[0:len(ss) // 2]
+    s2 = ss[len(ss) // 2:]
+    print(s1)
+    a = AESCipher("password")
+    b = DESCipher("password")
+    t1 = a.encrypt(s1)
+    t2 = b.encrypt(s2)
+    file1 = open('files/file1.txt', 'w')
+    file2 = open('files/file2.txt', 'w')
+    file1.write(t1)
+    file2.write(t2)
+
+
+if __name__ == '__main__':)
